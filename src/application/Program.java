de6b345e07744afe8d20c9638d0ce22389bb321e
JonasXPX <jonasdefarias@hotmail.com>
2020-09-09 20:54:03 -0300

    no ref

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,97 +2,121 @@ package application;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
-import java.util.Scanner;
+import java.time.LocalDate;
+import java.util.*;
+import java.util.logging.Logger;
 
 import entities.ContratosEHoras;
 import entities.Departamento;
 import entities.Funcionarios;
 import entities.enums.Level;
 
+import javax.swing.*;
+
+import static java.util.logging.Level.*;
+
 public class Program {
+
+	public static Logger logger = Logger.getLogger("Program");
 
 	public static void main(String[] args) throws ParseException {
 		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
-		Funcionarios fun = new Funcionarios();
+		Scanner scanner = new Scanner(System.in);
+
 		List<Funcionarios> list = new ArrayList<>();
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-		System.out.print("Digite o numero de fucionarios a serem registrados ");
-		int n = sc.nextInt();
+		System.out.println("Digite o numero de fucionarios a serem registrados ");
+		int n = scanner.nextInt();
 
 		for (int i = 0; i < n; i++) {
+			Funcionarios fun;
+			int ids = requestIntInput("Digite o ID");
 
-			System.out.print("Digite o id");
-			int ids = sc.nextInt();
 //			Funcionarios fuId = list.stream().filter(x -> x.getId().equals(ids)).findFirst().orElse(null);
 			while (temId(list, ids)) {
-				System.out.println("Id ja existe");
-				ids = sc.nextInt();
+				ids = requestIntInput("ID Já Existente");
 			}
 
-			System.out.print("Entre com o nome do departamento ");
-			String nomeDep = sc.nextLine();
-
-			System.out.println("Dados do trabalhador");
-			System.out.print("Nome ");
-			String nomes = sc.nextLine();
+			String nomeDep = requestStringInput("Entre com o nome do departamento");
+			String nomes = requestStringInput("Dados do trabalhador, nome: ");
 
 			System.out.print("Idade");
-			int idade = sc.nextInt();
+			int idade = requestIntInput(scanner);
+
 
 			System.out.print("level : ");
-			sc.nextLine();
-			String levels = sc.nextLine();
+			scanner.nextLine();
+			String levels = scanner.nextLine();
 
 			System.out.print("Salario ");
-			double salario = sc.nextDouble();
+			double salario = scanner.nextDouble();
 
 			fun = new Funcionarios(ids, nomes, idade, Level.valueOf(levels), salario, new Departamento(nomeDep));
 			list.add(fun);
-
 		}
 
+		int idFuncionario = requestIntInput("Informe o funcionário para adicionar contratos");
+		Funcionarios fun = list.stream().filter(funcionarios -> funcionarios.getId() == idFuncionario).findFirst().get();
+
 		System.out.println("Quantos contratos deseja ?");
-		int nu = sc.nextInt();
+		int nu = scanner.nextInt();
 
 		for (int i = 0; i < nu; i++) {
 			System.out.print("Data ");
-			Date data = sdf.parse(sc.next());
+			Date data = sdf.parse(scanner.next());
 			System.out.print("Valor horas");
-			double valorHoras = sc.nextDouble();
+			double valorHoras = scanner.nextDouble();
 			System.out.print("Duracao");
-			int horas = sc.nextInt();
+			int horas = scanner.nextInt();
 			ContratosEHoras contrato = new ContratosEHoras(data, valorHoras, horas);
 			fun.addContratos(contrato);
 		}
 		System.out.println("Entre com o id que deseja ver o total");
-		int id = sc.nextInt();
+		int id = scanner.nextInt();
 
 		System.out.println("Entre com o Mes e Ano");
-		String mesAno = sc.next();
+		String mesAno = scanner.next();
 		int mes = Integer.parseInt(mesAno.substring(0, 2));
 		int ano = Integer.parseInt(mesAno.substring(3));
 
-		if (fun.getId() == id) {
-			System.out.println("Nome " + fun.getNome());
-			System.out.println("Departamento " + fun.getDepartamento().getNome());
-			System.out.println("Level : " + fun.getLevel());
-			System.out.println("Ganhos " + mesAno + ", " + fun.ganhos(ano, mes));
-		} else {
-			System.out.println("Este id nao existe");
+		Optional<Funcionarios> optinalFuncionario = list.stream()
+				.filter(funcionario -> funcionario.getId() == id)
+				.findFirst();
 
+		if(!optinalFuncionario.isPresent()) {
+			System.out.println("Funcionário não cadastrado");
 		}
 
-		sc.close();
+		Funcionarios itemFuncionario = optinalFuncionario.get();
+
+		System.out.println("Nome " + itemFuncionario.getNome());
+		System.out.println("Departamento " + itemFuncionario.getDepartamento().getNome());
+		System.out.println("Level : " + itemFuncionario.getLevel());
+		System.out.println("Ganhos " + mesAno + ", " + itemFuncionario.ganhos(ano, mes));
+
+		scanner.close();
 	}
 
 	public static boolean temId(List<Funcionarios> list, int id) {
 		Funcionarios funs = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
 		return funs != null;
+	}
+
+	private static int requestIntInput(Scanner scanner) {
+		return scanner.nextInt();
+	}
+
+	private static String requestStringInput(String mensagem, Scanner scanner) {
+		System.out.println(mensagem);
+		return scanner.next();
+	}
+
+	private static String requestStringInput(String mensagem) {
+		return JOptionPane.showInputDialog(mensagem);
+	}
+
+	private static int requestIntInput(String mensagem) {
+		return Integer.parseInt(JOptionPane.showInputDialog(mensagem));
 	}
 }
